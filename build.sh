c17a056309293e1c6d7769f08a001c2b1689f21c
unk217 <unk00@hotmail.com>
2023-11-26 16:06:29 -0500

    r

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-cd .. python manage.py collectstatic --no-input
-cd .. python manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
