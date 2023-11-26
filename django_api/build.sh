7afea99a6f309e1746be4b9d7970b6922e10e270
unk217 <unk00@hotmail.com>
2023-11-26 15:54:55 -0500

    c

--- django_api/build.sh
+++ django_api/build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+cd .. python manage.py collectstatic --no-input
+cd .. python manage.py migrate
